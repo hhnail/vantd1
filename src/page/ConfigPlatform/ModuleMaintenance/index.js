9f600b789796b2dbbb07164a7610f1c77f4ce9e2
hhnail <hhnail@163.com>
2022-04-23 17:52:44 +0800

    查询模块信息

--- src/page/ConfigPlatform/ModuleMaintenance/index.js
+++ src/page/ConfigPlatform/ModuleMaintenance/index.js
@@ -1,94 +1,34 @@
 import {Table, Switch, Space} from 'antd';
+import {useEffect, useState} from "react";
+import {getModuleInfo} from "../../../service/commonService";
 
 const columns = [
     {
-        title: 'Name',
+        title: '名称',
         dataIndex: 'name',
         key: 'name',
     },
     {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-        width: '12%',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
+        title: '路由地址',
+        dataIndex: 'routingAddress',
         width: '30%',
-        key: 'address',
+        key: 'routingAddress',
     },
 ];
-
-const data = [
-    {
-        key: 1,
-        name: 'John Brown sr.',
-        age: 60,
-        address: 'New York No. 1 Lake Park',
-        children: [
-            {
-                key: 11,
-                name: 'John Brown',
-                age: 42,
-                address: 'New York No. 2 Lake Park',
-            },
-            {
-                key: 12,
-                name: 'John Brown jr.',
-                age: 30,
-                address: 'New York No. 3 Lake Park',
-                children: [
-                    {
-                        key: 121,
-                        name: 'Jimmy Brown',
-                        age: 16,
-                        address: 'New York No. 3 Lake Park',
-                    },
-                ],
-            },
-            {
-                key: 13,
-                name: 'Jim Green sr.',
-                age: 72,
-                address: 'London No. 1 Lake Park',
-                children: [
-                    {
-                        key: 131,
-                        name: 'Jim Green',
-                        age: 42,
-                        address: 'London No. 2 Lake Park',
-                        children: [
-                            {
-                                key: 1311,
-                                name: 'Jim Green jr.',
-                                age: 25,
-                                address: 'London No. 3 Lake Park',
-                            },
-                            {
-                                key: 1312,
-                                name: 'Jimmy Green sr.',
-                                age: 18,
-                                address: 'London No. 4 Lake Park',
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        key: 2,
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-    },
-];
-
 
 
 export default function ModuleMaintenance() {
 
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getModuleInfo().then(res => {
+            const {data} = res
+            console.log(data)
+            setData(data)
+        })
+    }, [])
 
 
     return (
--- src/service/commonService.js
+++ src/service/commonService.js
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+/**
+ * 获取头部菜单
+ */
 export function getHeaderMenu(){
     return axios.post('/vapi/getHeaderMenu')
 }
+
+
+/**
+ * 获取模块信息
+ */
+export function getModuleInfo(){
+    return axios.post('/vapi/getModule')
+}
+
