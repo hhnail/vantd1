4382faaf68e7fd99b3e4a0eacaebf602b6119fed
lsHhnail <hhnail@163.com>
2022-05-10 16:00:40 +0800

    朗新修改git user后，commit。前端树节点类别

--- src/enums/treeNodeType.js
+++ src/enums/treeNodeType.js
@@ -1,10 +1,18 @@
 export const TREE_NODE_TYPE = {
     // 顶部菜单
-    HEADER_MENU : {
-        value : "HEADER_MENU",
+    HEADER_MENU: {
+        value: "HEADER_MENU",
     },
     // 侧边栏菜单
-    SIDEBAR : {
-        value : "SIDEBAR",
+    SIDEBAR: {
+        value: "SIDEBAR",
+    },
+    // 表分组
+    TABLE_GROUP: {
+        value: "TABLE_GROUP",
+    },
+    // 角色分组
+    ROLE_GROUP: {
+        value: "ROLE_GROUP",
     },
 }
